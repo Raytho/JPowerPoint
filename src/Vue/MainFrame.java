4325a6e8622fd971f7a2637b8b4a56ef41e808c4
ISEN <ISEN@ISENMIC-N20T23U>
2016-01-27 11:07:26 +0100

    My Commit

---
+++ src/Vue/MainFrame.java
@@ -0,0 +1,11 @@
+package Vue;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+    public MainFrame() {
+        super("PowerPoint");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(1000,750);
+        this.setVisible(true);
+    }
+}
---
+++ src/Vue/main.java
@@ -0,0 +1,10 @@
+package Vue;
+
+public class main {
+
+    
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+    }
+    
+}
