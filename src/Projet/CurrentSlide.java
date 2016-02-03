6eeb6456bf28cbd5120ea10cf2766f853c63915e
ISEN <ISEN@ISENMIC-N20T23U>
2016-02-03 09:48:03 +0100

    My Commit

--- src/Projet/CurrentSlide.java
+++ src/Projet/CurrentSlide.java
@@ -1,16 +1,22 @@
 package Projet;
+import Observe.Observer;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-public class CurrentSlide extends JPanel{
+public class CurrentSlide extends JPanel implements Observer{
     private Slide slide;
     
     public CurrentSlide(Slide slide) {
         this.slide = slide;
         this.setPreferredSize(new Dimension(1000,700));
         this.setBorder(BorderFactory.createLineBorder(Color.black));
+        
+    }
+
+    @Override
+    public void update(Application app) {
         
     }
 }
