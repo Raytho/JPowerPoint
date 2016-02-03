27705fd510bd7c75a7fc38fdfde3cab26d4a3034
ISEN <ISEN@ISENMIC-N20T23U>
2016-02-03 09:37:59 +0100

    My Commit

--- src/Projet/main.java
+++ src/Projet/main.java
@@ -11,7 +11,7 @@ public class main {
 
     public static void main(String[] args) {
         Application app = new Application();
-        CurrentSlide currentSlide = new CurrentSlide(app.getSlides().get(0));
+        
         Miniatures mini = new Miniatures(app);
         app.addObserver(mini);
         
@@ -25,12 +25,14 @@ public class main {
         scroll.setPreferredSize(new Dimension(200,200));
         leftPanel.add(scroll, BorderLayout.CENTER);
         leftPanel.add(mini, BorderLayout.CENTER);
+        frame.add(leftPanel, BorderLayout.WEST);
         
+        CurrentSlide currentSlide = new CurrentSlide(app.getSlides().get(0));
         JPanel rightPanel = new JPanel();
         rightPanel.add(currentSlide);
         rightPanel.setBorder(BorderFactory.createLineBorder(Color.black));
         frame.add(rightPanel, BorderLayout.CENTER);
-        frame.add(leftPanel, BorderLayout.WEST);
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
         frame.setVisible(true);
